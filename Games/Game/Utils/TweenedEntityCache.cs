edb0da021db8b8e72cc2ac7cf042c5b208825e3d
Tzach Shabtay <tzachshabtay@gmail.com>
2017-10-15 19:42:35 -0400

    Replace queue locks with ConcurrentQueue

--- Games/Game/Utils/TweenedEntityCache.cs
+++ Games/Game/Utils/TweenedEntityCache.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using AGS.API;
 using AGS.Engine;
@@ -11,7 +12,7 @@ namespace Game.Utils
         public delegate Tween CreateTweenForEntity(T entity);
         public delegate void OnTweenCompleteHandler(T entity);
 
-        private Queue<T> _avail = new Queue<T>();
+        private ConcurrentQueue<T> _avail = new ConcurrentQueue<T>();
         private LinkedList<(T entity, Tween tween)> _inUse = new LinkedList<(T, Tween)>();
 
         public TweenedEntityCache(IEvent repExecEvent)
@@ -23,47 +24,37 @@ namespace Game.Utils
 
         public void Add(T e)
         {
-            lock (_avail)
-            {
-                _avail.Enqueue(e);
-            }
+            _avail.Enqueue(e);
         }
 
         public T BeginUsing(CreateTweenForEntity createTween)
         {
-            lock (_avail)
+            if (!_avail.TryDequeue(out T e))
+                return null;
+            Tween tween = createTween(e);
+            if (tween == null)
             {
-                if (_avail.Count == 0)
-                    return null;
-                T e = _avail.Dequeue();
-                Tween tween = createTween(e);
-                if (tween == null)
-                {
-                    _avail.Enqueue(e); // put it back
-                    return null;
-                }
-                _inUse.AddLast((e, tween));
-                return e;
+                _avail.Enqueue(e); // put it back
+                return null;
             }
+            _inUse.AddLast((e, tween));
+            return e;
         }
 
         private void OnRepeatedlyExecute()
         {
-            lock (_avail)
+            var node = _inUse.First;
+            while (node != null)
             {
-                var node = _inUse.First;
-                while (node != null)
+                var next = node.Next;
+                var (e, tween) = node.Value;
+                if (tween.Task.IsCompleted)
                 {
-                    var next = node.Next;
-                    var (e, tween) = node.Value;
-                    if (tween.Task.IsCompleted)
-                    {
-                        _inUse.Remove(node);
-                        OnTweenComplete(e);
-                        _avail.Enqueue(e);
-                    }
-                    node = next;
+                    _inUse.Remove(node);
+                    OnTweenComplete(e);
+                    _avail.Enqueue(e);
                 }
+                node = next;
             }
         }
     }
