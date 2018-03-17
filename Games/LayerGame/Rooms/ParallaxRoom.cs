205cbcb3ebc65cd73f56f492ac92c5ee69e92506
Ivan Mogilko <ikm_spb@yahoo.com>
2018-03-17 19:02:16 +0300

    LayerGame: pre-foreground layer and room bounds

--- Games/LayerGame/Rooms/ParallaxRoom.cs
+++ Games/LayerGame/Rooms/ParallaxRoom.cs
@@ -8,6 +8,8 @@ namespace LayerGame
     {
         private const string ROOM_ID = "ParallaxRoom";
         private IRenderLayer[] _backgroundLayers;
+        private IRenderLayer[] _foregroundLayers;
+        private RectangleF _roomBounds;
 
         public ParallaxRoom(IGame game) : base(game, ROOM_ID)
         {
@@ -25,27 +27,38 @@ namespace LayerGame
             IGameFactory f = _game.Factory;
             _room = f.Room.GetRoom(ROOM_ID);
 
-            PointF[] parallaxVals = new PointF[4]
+            PointF[] parallaxVals = new PointF[6]
             {
-                new PointF(0.01f, 0.2f), new PointF(0.15f, 0.4f), new PointF(0.1f, 0.6f), new PointF(0.3f, 0.8f)
+                new PointF(0f, 0.2f), new PointF(0.15f, 0.4f), new PointF(0.1f, 0.6f), new PointF(0.3f, 0.8f),
+                new PointF(1f, 1f), new PointF(2.4f, 2f)
             };
 
-            Point[] coordVals = new Point[4]
+            Point[] coordVals = new Point[6]
             {
-                new Point(-200, 0), new Point(-320, 250), new Point(-320, 100), new Point(-240, -50)
+                new Point(-200, 0), new Point(-320, 250), new Point(-320, 100), new Point(-240, -50),
+                new Point(0, -55), new Point (400, -120)
             };
 
             _backgroundLayers = new IRenderLayer[4];
             for (int i = 0, z = AGSLayers.Background.Z - 1; i < 4; ++i, --z)
             {
                 _backgroundLayers[i] = new AGSRenderLayer(z, parallaxVals[i]);
-                IObject o = await addTerra("layer1", $"parallax_l{i + 1}.png", coordVals[i].X, coordVals[i].Y);
+                IObject o = await addTerra($"layer{i}", $"parallax_l{i + 1}.png", coordVals[i].X, coordVals[i].Y);
                 o.RenderLayer = _backgroundLayers[i];
             }
 
-            int rx = _game.Settings.VirtualResolution.Width;
-            IObject foreground = await addTerra("foreground", "parallax_l5.png", 0, -55);
-            _room.RoomLimitsProvider = AGSRoomLimits.Infinite;
+            IObject foreground = await addTerra("foreground", "parallax_l5.png", coordVals[4].X, coordVals[4].Y);
+
+            _foregroundLayers = new IRenderLayer[1];
+            for (int i = 5, l = 0, z = AGSLayers.Foreground.Z - 1; i < 6; ++i, --z)
+            {
+                _foregroundLayers[l] = new AGSRenderLayer(z, parallaxVals[i]);
+                IObject o = await addTerra($"layer{i}", $"parallax_l{i + 1}.png", coordVals[i].X, coordVals[i].Y);
+                o.RenderLayer = _foregroundLayers[l];
+            }
+
+            _roomBounds = new RectangleF(foreground.X, foreground.Y, foreground.Width, 400f - foreground.Y);
+            _room.RoomLimitsProvider = AGSRoomLimits.Custom(_roomBounds);
 
             _game.Events.OnRepeatedlyExecute.Subscribe(onRepExec);
 
@@ -56,6 +69,8 @@ namespace LayerGame
         {
             var input = _game.Input;
             var view = _game.State.Viewport;
+            int rx = _game.Settings.VirtualResolution.Width;
+            int ry = _game.Settings.VirtualResolution.Height;
             float viewspeed = 100f * (float)args.DeltaTime;
             if (input.IsKeyDown(Key.Left))
                 view.X -= viewspeed;
@@ -65,6 +80,8 @@ namespace LayerGame
                 view.Y += viewspeed;
             if (input.IsKeyDown(Key.Down))
                 view.Y -= viewspeed;
+            view.X = MathUtils.Clamp(view.X, _roomBounds.X, _roomBounds.X + _roomBounds.Width - rx);
+            view.Y = MathUtils.Clamp(view.Y, _roomBounds.Y, _roomBounds.Y + _roomBounds.Height - ry);
         }
     }
 }
